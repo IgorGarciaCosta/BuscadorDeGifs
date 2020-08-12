a53336e3a227d8d4a71271797d281012b964cf7a
IgorGarciaCosta <igonildo7@gmail.com>
2020-08-12 11:43:21 -0300

    projeto

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:buscador_gifs/ui/home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(hintColor: Colors.white),
+  ));
+}
+
+
+
